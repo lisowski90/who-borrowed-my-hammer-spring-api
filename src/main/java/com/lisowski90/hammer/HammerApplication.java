4e8b158b91697bd5d717142a277c08a5a183e12c
lisowski90 <lisowski1990@gmail.com>
2020-10-13 12:29:24 +0200

    build

---
+++ src/main/java/com/lisowski90/hammer/HammerApplication.java
@@ -0,0 +1,13 @@
+package com.lisowski90.hammer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HammerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HammerApplication.class, args);
+	}
+
+}
